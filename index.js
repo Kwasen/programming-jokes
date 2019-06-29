268d9a748dc8b13d3bbfd5302c6159c18cd76296
Mabweh <imabweh@gmail.com>
2019-06-29 22:21:31 +0800

    Remove Jquery, improve ui

--- index.js
+++ index.js
@@ -1,34 +1,44 @@
 var jokeDOM = document.getElementById("jokePanel");
 var jokeGetterDOM = document.getElementById("getJokeBtn");
-var myFooter = document.getElementById("myFooter");
 
 jokeDOM.innerHTML = "Hello Foo";
 
-const jokes = ['["Hip", "hip"] = array', 'why did the programmer leave his job? \n Because he could not get arrays','what is a programmers favourite hangout place? \n foo bar', 'what do you call a programmer from Finland? \n Nerdic'];
+const jokes = [
+  '["Hip", "hip"] = array',
+  'why did the programmer leave his job? \n Because he could not get arrays',
+  'what is a programmers favourite hangout place? \n foo bar',
+  'what do you call a programmer from Finland? \n Nerdic'
+];
 
 function getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function getJoke (index){
+function getJoke(index) {
 
-	if(typeof jokes[index] === 'undefined') {
-		return 'Jokes on you. I am undefined';
-	} else {
-		return jokes[index];
-	}
+  if (typeof jokes[index] === 'undefined') {
+    return 'Jokes on you. I am undefined';
+  } else {
+    return jokes[index];
+  }
 }
 
-// footer styling
-var footerPaddingTop = Math.floor(window.innerHeight - (window.innerHeight * 0.10));
-console.log('footerPaddingTop: ', footerPaddingTop);
-myFooter.style.marginTop = footerPaddingTop + 'px';
-// output
+jokeGetterDOM.addEventListener('click', function (event) {
+  if (this.classList) {
+    this.classList.toggle('animated');
+    this.classList.toggle('tada');
+  } else {
+    // probably an IE9 browser. TODO: TEST THIS
+    var classes = this.className.split(" ");
+    var i = classes.indexOf("animated tada");
 
-$(jokeGetterDOM).click(function(){
-  $(this).toggleClass('animated tada');
-  var randomIndex = getRandomInt(0, jokes.length);  
-  jokeDOM.innerHTML  = getJoke(randomIndex);
-  myFooter.style.backgroundColor = "red";
+    if (i >= 0)
+      classes.splice(i, 1);
+    else
+      classes.push("animated tada");
+      this.className = classes.join(" ");
+  }
+
+  var randomIndex = getRandomInt(0, jokes.length);
+  jokeDOM.innerHTML = getJoke(randomIndex);
 });
-
